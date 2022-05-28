9b16538ecdb32007f15143132113bbec83ab137a
Federico Slongo <73640124+FedericoSlongo@users.noreply.github.com>
2022-05-28 13:48:31 +0200

    Update install_script_grub.sh

--- grub/install_script_grub.sh
+++ grub/install_script_grub.sh
@@ -2,10 +2,10 @@
 
 echo Copying the theme in the right folder
 cp -r ./themes /boot/grub
-echo The themes was copied succefully :D
+echo If there are no error the themes was copied succefully :D
 
 echo Copying the grub file
 cp ./default/grub /etc/default
-echo Grub file copied succefully
+echo If there are no error grub file copied succefully
 
 echo Executing grub update and it should work
