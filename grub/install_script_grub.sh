ece1c33578770dd7cbe8924589a320a93be8461a
Federico Slongo <73640124+FedericoSlongo@users.noreply.github.com>
2022-05-28 13:40:27 +0200

    Add files via upload

---
+++ grub/install_script_grub.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo Copying the theme in the right folder
+cp -r ./themes /home/federico/Documents
+echo The themes was copied succefully :D
+
+echo Copying the grub file
+cp ./default/grub /home/federico/Documents
+echo Grub file copied succefully
+
+echo Executing grub update and it should work
+
+
+
