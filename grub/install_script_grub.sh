2e11400f84852839201e57acd28ebee1995fcd20
Federico Slongo <73640124+FedericoSlongo@users.noreply.github.com>
2022-05-28 14:05:51 +0200

    The script is complete now

--- grub/install_script_grub.sh
+++ grub/install_script_grub.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
+NC='\033[0m' # No Color
+CYAN='\033[0;36m' # Cyan Color
 
+#Checking if the script is run whit sudo
 if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
+  then printf "${CYAN}Please run as root \n"
   exit
 fi
 
-
-echo Copying the theme in the right folder
+#Copying the theme folder
+printf "${CYAN}Copying the theme in the right folder \n"
 cp -r ./themes /boot/grub
-echo If there are no error the themes was copied succefully :D
+printf "${CYAN}If there are no error the themes was copied succefully :D \n"
 
-echo Copying the grub file
+#Copying the grub file
+printf "${CYAN}Copying the grub file \n"
 cp ./default/grub /etc/default
-echo If there are no error grub file copied succefully
+printf "${CYAN}If there are no error grub file copied succefully \n"
 
-echo Executing grub update and it should work
+#Executing the sudo grub-update command
+printf "${CYAN}Executing grub update ${NC}\n"
+sudo update-grub
+printf "${CYAN}Grub updated \n "
